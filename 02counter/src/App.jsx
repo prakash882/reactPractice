96cecd1cfdb5d50a23e137fba223f53b78304652
prakash882 <pt422194@gmail.com>
2025-05-04 12:44:48 +0545

    added loop

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,28 +5,32 @@ import './App.css'
 
 function App() {
 
-  let [counter, setCounter] =  useState(15)
+  let [counter, setCounter] =  useState(0)
 
   //let counter = 15
 
   const addValue = () => {
     //counter = counter + 1
+    if (counter < 20){
     setCounter(counter + 1)
+    }
   } 
 
   const removeValue = () =>{
+    if (counter > 0){
     setCounter(counter - 1)
+    }
   }
 
   return (
     <>
-      <h1>Chai aur react</h1>
+      <h1>Counter Project</h1>
       <h2>Counter Value: {counter} </h2>
-      <button onClick={addValue}>Add Value {counter}</button>
+      <button onClick={addValue}>Add Value</button>
+      <br />
       <br />
       <button
-      onClick={removeValue}>remove Value {counter}</button>
-      <p>footer: {counter}</p>
+      onClick={removeValue}>Remove Value</button>
     </>
   )
 }
