c5d6199410fcebb95045f0e6926044241b830b12
prakash882 <pt422194@gmail.com>
2025-05-07 15:28:21 +0545

    certain change

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -19,13 +19,13 @@ export default function Header() {
                             to="#"
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Log in
+                            Log In
                         </Link>
                         <Link
                             to="#"
                             className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Get started
+                            Get Started
                         </Link>
                     </div>
                     <div
